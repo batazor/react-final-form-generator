8b5fbd13c9f66b9ebfda798a1c3ba4a8679a6245
Victor Login <batazor111@gmail.com>
2018-10-15 17:45:54 +0300

    add storybook

--- src/Component/Checkbox.js
+++ src/Component/Checkbox.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Checkbox from '@material-ui/core/Checkbox';
+import Checkbox from '@material-ui/core/Checkbox'
 
 export default ({
   input: {
---
+++ src/stories/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Form from '../index'
+
+const simple = [
+  {
+    name: 'email',
+    label: 'Email',
+    type: 'TextField',
+  },
+]
+
+const complex = [
+  {
+    name: 'email',
+    label: 'Email',
+    type: 'TextField',
+  },
+  {
+    name: 'password',
+    label: 'Password',
+    type: 'TextField',
+  },
+  {
+    name: 'retryPassword',
+    label: 'Retry password',
+    type: 'TextField',
+  },
+  {
+    name: 'language',
+    label: 'Language',
+    type: 'Select',
+    option: [
+      {
+        value: 'ru_RU',
+        label: 'Russia',
+      },
+      {
+        value: 'en_GB',
+        label: 'English',
+      },
+    ],
+  },
+]
+
+storiesOf('Form', module)
+  .add('Simple', () => (
+    <Form
+      id="form"
+      fields={simple}
+
+      onSubmit={action('onSubmit')}
+    />
+  ))
+  .add('Complex', () => (
+    <Form
+      id="form"
+      fields={complex}
+
+      onSubmit={action('onSubmit')}
+    />
+  ))
